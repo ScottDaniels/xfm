f6df37c66043161b180cfe8af136641b553ea739
Scott Daniels <escottdaniels@gmail.com>
2015-09-06 01:17:40 -0400

    cvt CVS to git: initial stash of doc.

---
+++ doc/master.mk
@@ -0,0 +1,22 @@
+# global mk patterns
+
+MKSHEL = ksh
+
+%.txt: %xfm
+	tfm ${prereq%% *} $target
+
+%.html: %.xfm
+	hfm ${prereq%% *} $target
+	
+%.txt: %.xfm
+	tfm ${prereq%% *} $target
+
+%.ps: %.xfm
+	pfm ${prereq%% *} $target
+
+# rfm is deprecated, but we'll keep this
+%.rtf: %.xfm
+	rfm ${prereq%% *} $target
+	
+%.pdf::	%.ps
+	gs -dBATCH  -dNOPROMPT -dNOPAUSE -sDEVICE=pdfwrite ${prereq%% }
