a9acfcec4c54355d5cb8799bae53dd3cd166de6e
Scott Daniels <escottdaniels@gmail.com>
2016-01-31 17:14:09 -0500

    Removed non-postscript license from init.ps file.

--- src/pfm/init_ps.c
+++ src/pfm/init_ps.c
@@ -1,33 +1,3 @@
-/*
-=================================================================================================
-	(c) Copyright 1995-2015 By E. Scott Daniels. All rights reserved.
-
-	Redistribution and use in source and binary forms, with or without modification, are
-	permitted provided that the following conditions are met:
-
-		1. Redistributions of source code must retain the above copyright notice, this list of
-		conditions and the following disclaimer.
-
-		2. Redistributions in binary form must reproduce the above copyright notice, this list
-		of conditions and the following disclaimer in the documentation and/or other materials
-		provided with the distribution.
-
-	THIS SOFTWARE IS PROVIDED BY E. Scott Daniels ``AS IS'' AND ANY EXPRESS OR IMPLIED
-	WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND
-	FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL E. Scott Daniels OR
-	CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
-	CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
-	SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON
-	ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
-	NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF
-	ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
-
-	The views and conclusions contained in the software and documentation are those of the
-	authors and should not be interpreted as representing official policies, either expressed
-	or implied, of E. Scott Daniels.
-=================================================================================================
-*/
-
 AFIwrite( ofile, " /box { 3 index 3 index moveto 3 index 1 index lineto  1 index 1 index lineto 1 index 3 index lineto  closepath stroke pop pop pop pop } def\n" );
 AFIwrite( ofile, " /rightxy { dup stringwidth pop 3 index exch sub 2 index moveto show pop pop } def /showstack {  np 0 eq    { show }  {   0 1 np 1 sub\n" );
 AFIwrite( ofile, "  { pop  aload pop  dup -1 eq   { pop }  { setrgbcolor }  ifelse  /fs exch def  /ya exch def  exch  findfont fs scalefont setfont\n" );
