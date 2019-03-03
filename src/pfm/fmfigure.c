2092bcf574df3fdbd8c2db862ad8682a8c390f49
Scott Daniels <escottdaniels@gmail.com>
2019-03-03 14:36:29 -0500

    Add v= parm to figure command

--- src/pfm/fmfigure.c
+++ src/pfm/fmfigure.c
@@ -1,20 +1,20 @@
 /*
 All source and documentation in the xfm tree are published with the following open source license:
-Contributions to this source repository are assumed published with the same license. 
+Contributions to this source repository are assumed published with the same license.
 
 =================================================================================================
 	(c) Copyright 1995-2015 By E. Scott Daniels. All rights reserved.
 
 	Redistribution and use in source and binary forms, with or without modification, are
 	permitted provided that the following conditions are met:
-	
-   		1. Redistributions of source code must retain the above copyright notice, this list of
-      		conditions and the following disclaimer.
-		
-   		2. Redistributions in binary form must reproduce the above copyright notice, this list
-      		of conditions and the following disclaimer in the documentation and/or other materials
-      		provided with the distribution.
-	
+
+ 		1. Redistributions of source code must retain the above copyright notice, this list of
+    		conditions and the following disclaimer.
+
+ 		2. Redistributions in binary form must reproduce the above copyright notice, this list
+     		of conditions and the following disclaimer in the documentation and/or other materials
+    		provided with the distribution.
+
 	THIS SOFTWARE IS PROVIDED BY E. Scott Daniels ``AS IS'' AND ANY EXPRESS OR IMPLIED
 	WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND
 	FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL E. Scott Daniels OR
@@ -24,7 +24,7 @@ Contributions to this source repository are assumed published with the same lice
 	ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
 	NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF
 	ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
-	
+
 	The views and conclusions contained in the software and documentation are those of the
 	authors and should not be interpreted as representing official policies, either expressed
 	or implied, of E. Scott Daniels.
@@ -32,16 +32,16 @@ Contributions to this source repository are assumed published with the same lice
 */
 
 
-#include <stdio.h>     
+#include <stdio.h>
 #include <stdlib.h>
-#include <fcntl.h>    
-#include <ctype.h>   
-#include <string.h> 
+#include <fcntl.h>
+#include <ctype.h>
+#include <string.h>
 #include <memory.h>
 #include <time.h>
 
 #include "symtab.h"		/* our utilities/tools */
-#include "../afileio/afidefs.h"   
+#include "../afileio/afidefs.h"
 
 
 #include "fmconst.h"               /* constant definitons */
@@ -63,8 +63,8 @@ Contributions to this source repository are assumed published with the same lice
 *             the logic if a figure table will ever be generated. Figure
 *             numbers are the section number followed by a sequential number.
 *
-*				If 'nosect' is given on the command, then the command is 
-*				used just to set the flags which control whether or not 
+*				If 'nosect' is given on the command, then the command is
+*				used just to set the flags which control whether or not
 *				section style numbering is used when paragraph numbering is
 *				turned on.
 *   Parms:    None.
@@ -75,14 +75,14 @@ Contributions to this source repository are assumed published with the same lice
 *
 *				Two styles:
 *				.fg nosect
-*				.fg [s=size] [f=font] [n=number] [t=type] <text>
+*				.fg [s=size] [f=font] [v=var_name] [num=number] [t=type] <text>
 *					type is either table or figure, default is figure.
 *
 *   Modified:	30 Oct 1992 - To reduce figure text size by two from current size
 *				26 May 1993 - To reduce space between figure text lines.
 *				22 Feb 1994 - To ensure text font is set properly
 *			    13 Oct 2001 - To setup for fonts using the font blocks
-*				04 Mar 2014 - To allow for preallocation of figure numbers and 
+*				04 Mar 2014 - To allow for preallocation of figure numbers and
 *					to support their use on the .fg command
 *				17 Jul 2016 - Bring decls into the modern world.
 *				11 Aug 2017 - Add size (s=), font (f=), and nosect parm support.
@@ -99,8 +99,10 @@ extern void FMfigure( void )
 	int	tok_len;
 	char	*cp;
 	char	*type = "Figure";
-	int	*num_src;		/* pointer to source for table or figure number */
-	char	fbuf[100];	/* format buffer */
+	int	*num_src;				/* pointer to source for table or figure number */
+	char	fbuf[100];			/* format buffer */
+	char	vbuf[100];			// build .dv command in if vname given
+	char*	vname = NULL;		// var name to put fig number into
 
 
 	tok_len = FMgetparm( &buf );				// get early so we can check for nosect command and bail before any real work
@@ -108,16 +110,16 @@ extern void FMfigure( void )
 		flags3 &= ~F3_FIG_SECT_NUM;				// turn off section style even if para numbers are on
 		return;
 	}
-	
+
 	num_src = &fig;			/* default to using figure number */
 
 	FMflush( );            /* end current working line */
- 	FMfmt_add( );			// start the next line
+	FMfmt_add( );			// start the next line
 
 	oldspace = textspace;  /* save stuff we need to restore when leaving */
 	oldfont = curfont;     /* save old font for later */
 	oldsize = textsize;    /* save old size for end */
-	
+
 	flags2 |= F2_SETFONT;  /* force flush to select our figure font */
 	curfont = strdup( ffont == NULL ? curfont : ffont );   							// default to figure font if there, text font if not, f= overrides later
 
@@ -134,7 +136,7 @@ extern void FMfigure( void )
 				}
 				break;
 
-			case 'n':			// use this figure number
+			case 'n':			// n[um]=n  or na[me]=name
 				if( *(cp + 1) ) {
 					fnum = atoi( cp+1 );
 				}
@@ -145,7 +147,7 @@ extern void FMfigure( void )
 					textsize = atoi( cp+1 );
 				}
 				break;
-				
+		
 			case 't':			// set type (figure or table)
 				if( *(cp+1) == 't' )
 				{
@@ -153,13 +155,19 @@ extern void FMfigure( void )
 					num_src = &table_number;		// refrence the correct table number if needed
 				}
 				break;
+
+			case 'v':			// var-name
+				if( *(cp + 1) ) {
+					vname = strdup( cp+1 );
+				}
+				break;
 		}
 
-		tok_len = FMgetparm( &buf );	
+		tok_len = FMgetparm( &buf );
 	}
 
- 	FMfmt_add();				/* must push the size on the stack */
-	
+	FMfmt_add();				/* must push the size on the stack */
+
 	if( fnum < 0 )				// not set on the command line
 	{
 		fnum = *num_src;		// pull from proper spot and bump up
@@ -167,9 +175,15 @@ extern void FMfigure( void )
 	}
 
 	if( (flags & PARA_NUM) && (flags3 & F3_FIG_SECT_NUM) ) {                      /* if numbering the paragraphs, add it to the number */
- 		snprintf( fbuf, sizeof( fbuf ), "%s %d-%d: ", type, pnum[0], fnum );   
+		snprintf( fbuf, sizeof( fbuf ), "%s %d-%d: ", type, pnum[0], fnum );
+		if( vname ) {
+			snprintf( vbuf, sizeof( vbuf ), ".dv %s %d-%d :", vname, pnum[0], fnum );
+		}
 	} else {
- 		snprintf( fbuf, sizeof( fbuf ), "%s %d: ", type, fnum );   /* gen fig number */
+		snprintf( fbuf, sizeof( fbuf ), "%s %d: ", type, fnum );   /* gen fig number */
+		if( vname ) {
+			snprintf( vbuf, sizeof( vbuf ), ".dv %s %d :", vname, fnum );
+		}
 	}
 
 
@@ -177,16 +191,21 @@ extern void FMfigure( void )
 	while( tok_len > 0 )		// add tokens, first already in the buffer
 	{
 		FMaddtok( buf, tok_len );                    /* add it to the output buffer */
-		tok_len = FMgetparm( &buf );	
+		tok_len = FMgetparm( &buf );
 	}
 
 	FMflush( );                             /* send user line on the way */
 
 	flags2 |= F2_SETFONT;  /* next flush will restore the font */
 
+	if( vname ) {
+		AFIpushtoken( fptr->file, vbuf );		// cause the desired variable to be set
+		free( vname );
+	}
+
 	free( curfont );
 	textspace = oldspace;  /* restore original text space value */
 	textsize  = oldsize;   /* restore the size that was set on entry */
 	curfont = oldfont;     /* restore the font set on entry */
- 	FMfmt_add( );		/* initial font info for next buffer */
-}  
+	FMfmt_add( );		/* initial font info for next buffer */
+}
